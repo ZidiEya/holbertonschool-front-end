23820ff58d17d67b16cd26339ecd03738fdcc5c1
ZidiEya <91125384+ZidiEya@users.noreply.github.com>
2023-01-27 23:46:35 +0100

    12. Binding

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,16 @@
+/*
+func to create an object named roomDimensions with the following three attributes:
+    width: 50
+    length: 100
+    getArea: function that returns the surface area of a the object using the width and length
+Create a variable named boundGetArea, that will bind the object roomDimensions to the getArea fun
+*/
+
+const roomDimensions = {
+    with: 50,
+    length: 100,
+    getArea() {return this.with * this.length;}
+}
+const area = roomDimensions.getArea;
+let boundGetArea = area.bind(roomDimensions)
+console.log(boundGetArea());
